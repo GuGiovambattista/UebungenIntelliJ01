566cd93904ce82f0fac151c0f417393654fefa2b
Guido Giovambattista <Giovambattista.Guido@swisscom.com>
2020-11-11 09:51:03 +0100

    1.	Programm liest das File "System.ini" 2.	die Message "Enter Username" als Output 3.	Username muss via Tastatur angeben werden 4.	Der Name wird in die Variable "userName" gespeichert 5.	 als Output gibt die String ("username ist " userName)

---
+++ src/ch/teko/prg/Main.java
@@ -0,0 +1,15 @@
+package ch.teko.prg;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter username: ");
+        String userName = myObj.nextLine();
+        System.out.println("Username is: " + userName);
+
+    }
+}
